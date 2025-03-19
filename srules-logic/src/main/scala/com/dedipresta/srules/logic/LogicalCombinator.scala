e8b81913b6695e9cdac51b52617166225e7f0354
Mathieu Prevel <mathieu.prevel@dedipresta.com>
2025-03-19 11:08:42 +0100

    fmt

--- srules-logic/src/main/scala/com/dedipresta/srules/logic/LogicalCombinator.scala
+++ srules-logic/src/main/scala/com/dedipresta/srules/logic/LogicalCombinator.scala
@@ -2,6 +2,6 @@ package com.dedipresta.srules.logic
 
 sealed trait LogicalCombinator
 object LogicalCombinator:
-  case object OneOf extends LogicalCombinator
-  case object AllOf extends LogicalCombinator
+  case object OneOf  extends LogicalCombinator
+  case object AllOf  extends LogicalCombinator
   case object NoneOf extends LogicalCombinator
--- srules-logic/src/main/scala/com/dedipresta/srules/logic/LogicalRule.scala
+++ srules-logic/src/main/scala/com/dedipresta/srules/logic/LogicalRule.scala
@@ -1,9 +1,10 @@
 package com.dedipresta.srules.logic
 
-import cats.data.NonEmptyList
 import com.dedipresta.srules.Expr
+
+import cats.data.NonEmptyList
 
 sealed trait LogicalRule
 object LogicalRule:
-  case class SimpleRule(name: String, rule: Expr) extends LogicalRule
+  case class SimpleRule(name: String, rule: Expr)                                                         extends LogicalRule
   case class CombinedRules(name: String, combinator: LogicalCombinator, rules: NonEmptyList[LogicalRule]) extends LogicalRule
